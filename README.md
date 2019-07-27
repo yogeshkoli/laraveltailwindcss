bae13c7177f2feecf584ef1a8900b8ec92b2c23d
Yogesh Koli <info@yogeshkoli.com>
2019-07-27 14:36:42 +0530

    Laravel app with tailwind css installed

---
+++ README.md
@@ -0,0 +1,29 @@
+# Laravel 5.8 Application with Tailwind CSS Installed 
+
+Tutorial Link - [How to use TailWindCSS with Laravel Framework](https://www.itechempires.com/2019/07/how-to-use-tailwindcss-with-laravel-framework/)
+
+### Installation
+
+```sh
+$ git clone https://github.com/yogeshkoli/laraveltailwindcss.git
+$ cd laraveltailwindcss
+$ composer install
+```
+
+Compiling Asset in development mode
+
+```sh
+$ npm install 
+$ npm run dev
+```
+Compiling Asset in Production mode
+
+```sh
+$ npm install 
+$ npm run prod
+```
+
+License
+----
+
+MIT
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+
+        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+
+        <style>
+            body{
+                padding-top: 40px;
+            }
+        </style>
+    </head>
+    <body>
+        <div class="flex mb-4">
+        <div class="w-1/3 h-12"></div>
+        <div class="w-1/3 h-12">
+            <div class="w-full max-w-xs">
+            <form class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+                <div class="mb-4">
+                <label class="block text-gray-700 text-sm font-bold mb-2" for="username">
+                    Username
+                </label>
+                <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" type="text" placeholder="Username">
+                </div>
+                <div class="mb-6">
+                <label class="block text-gray-700 text-sm font-bold mb-2" for="password">
+                    Password
+                </label>
+                <input class="shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="password" type="password" placeholder="******************">
+                <p class="text-red-500 text-xs italic">Please choose a password.</p>
+                </div>
+                <div class="flex items-center justify-between">
+                <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button">
+                    Sign In
+                </button>
+                <a class="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800" href="#">
+                    Forgot Password?
+                </a>
+                </div>
+            </form>
+            <p class="text-center text-gray-500 text-xs">
+                &copy;2019 iTech Empires. All rights reserved.
+            </p>
+            </div>
+        </div>
+        <div class="w-1/3 h-12"></div>
+        </div>
+    </body>
+</html>
